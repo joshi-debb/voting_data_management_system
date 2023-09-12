6cc183e9741951f5b9d4ed5edae2c26b5aeb6dfa
Josue Zuleta <mym.jayjay@gmail.com>
2023-09-12 06:10:51 -0600

    creando tablas temporales

--- src/controllers/cargatemp.js
+++ src/controllers/cargatemp.js
@@ -3,12 +3,21 @@ const config = require('../db/config')
 const mysql = require('mysql2/promise')
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join(__dirname, '../data/ciudadanos.csv');
+const filePath_Ciudadanos = path.join(__dirname, '../data/ciudadanos.csv');
+const filePath_Candidatos = path.join(__dirname, '../data/candidatos.csv');
+const filePath_Partidos = path.join(__dirname, '../data/partidos.csv');
+const filePath_Cargos = path.join(__dirname, '../data/cargos.csv');
+const filePath_Votaciones = path.join(__dirname, '../data/votaciones.csv');
+const filePath_Mesas = path.join(__dirname, '../data/mesas.csv');
+const filePath_Departamentos = path.join(__dirname, '../data/departamentos.csv');
+
 
 exports.crearTabtemp = async (req, res) => {
 
     const scriptCrearTablasTemp = `
+    
     -- Tabla temporal para Ciudadanos
+
     CREATE TEMPORARY TABLE BD1PY1.CIUDADANOTMP (
         dpi VARCHAR(100) NOT NULL,
         nombre VARCHAR(50) NOT NULL,
@@ -16,8 +25,74 @@ exports.crearTabtemp = async (req, res) => {
         direccion VARCHAR(200) NOT NULL,
         telefono VARCHAR(10) NOT NULL,
         edad VARCHAR(2) NOT NULL,
-        genero VARCHAR(1) NOT NULL
-    );`;
+        genero VARCHAR(1) NOT NULL,
+
+        PRIMARY KEY (dpi)
+    );
+    
+    -- Tabla temporal para Candidatos
+        
+    CREATE TEMPORARY TABLE BD1PY1.CANDIDATOTMP (
+        id_candidato INT NOT NULL AUTO_INCREMENT,
+        nombres_candidato VARCHAR(50) NOT NULL,
+        fecha_nacimiento DATE NOT NULL,
+        id_partido INT NOT NULL,
+        id_cargo INT NOT NULL,
+
+        PRIMARY KEY (id_candidato)
+    );
+
+    -- Tabla temporal para Partidos
+
+    CREATE TEMPORARY TABLE BD1PY1.PARTIDOTMP (
+        id_partido INT NOT NULL AUTO_INCREMENT,
+        nombre_partido VARCHAR(50) NOT NULL,
+        siglas VARCHAR(10) NOT NULL,
+        fundacion DATE NOT NULL,
+
+        PRIMARY KEY (id_partido)
+    );
+
+    -- Tabla temporal para Cargos
+
+    CREATE TEMPORARY TABLE BD1PY1.CARGOTMP (
+        id_cargo INT NOT NULL AUTO_INCREMENT,
+        cargo VARCHAR(100) NOT NULL,
+
+        PRIMARY KEY (id_cargo)
+    );
+
+    -- Tabla temporal para Votaciones
+
+    CREATE TEMPORARY TABLE BD1PY1.VOTACIONESTMP (
+        id_votacion INT NOT NULL AUTO_INCREMENT,
+        id_candidato INT NOT NULL,
+        dpi VARCHAR(100) NOT NULL,
+        id_mesa INT NOT NULL,
+        fechayhora DATE NOT NULL,
+
+        PRIMARY KEY (id_votacion)
+    );
+
+    -- Tabla temporal para Mesas
+
+    CREATE TEMPORARY TABLE BD1PY1.MESATMP (
+        id_mesa INT NOT NULL AUTO_INCREMENT,
+        id_departamento INT NOT NULL,
+
+        PRIMARY KEY (id_mesa)
+    );
+
+    -- Tabla temporal para Departamentos
+
+    CREATE TEMPORARY TABLE BD1PY1.DEPARTAMENTOTMP (
+        id_departamento INT NOT NULL AUTO_INCREMENT,
+        nombre_departamento VARCHAR(50) NOT NULL,
+
+        PRIMARY KEY (id_departamento)
+    );
+
+    `;
 
     
     try {
@@ -37,8 +112,11 @@ exports.crearTabtemp = async (req, res) => {
             await db.querywithoutclose(connection, sql, []);
         }
 
-        // carga de datos csv a tabla temporal
-        const datosClientes = fs.readFileSync(filePath, 'utf-8');
+        // carga de datos csv a tablas temporales
+        // ---------------------------------------------------------------------------------------------
+
+        // Cargar datos de ciudadanos
+        const datosClientes = fs.readFileSync(filePath_Ciudadanos, 'utf-8');
         const lines = datosClientes.split('\n');
         for (let i = 1; i < lines.length; i++) {
             const fields = lines[i].split(',');
@@ -55,10 +133,101 @@ exports.crearTabtemp = async (req, res) => {
             await db.querywithoutclose(connection, `INSERT INTO BD1PY1.CIUDADANOTMP (dpi, nombre, apellido, direccion, telefono, edad, genero ) VALUES (?, ?, ?, ?, ?, ?, ?)`, [dpi, nombre, apellido, direccion, telefono, edad, genero]);
         }
 
-        const tempClientesData = await db.querywithoutclose(connection, `SELECT * FROM BD1PY1.CIUDADANOTMP`, []);
-        console.log(tempClientesData);
+        // // Cargar datos de candidatos
+        // const datosCandidatos = fs.readFileSync(filePath_Candidatos, 'utf-8');
+        // const lines2 = datosCandidatos.split('\n');
+        // for (let i = 1; i < lines2.length; i++) {
+        //     const fields = lines2[i].split(',');
+        //     const id_candidato = fields[0];
+        //     const nombres_candidato = fields[1];
+        //     const fecha_nac = fields[2];
 
-        // // por ultimo pasamos los datos de la tabla temporal a la tabla clientes
+        //     //Convertir la fecha de nacimiento
+        //     const fecha_nacimiento = convertirFecha(fecha_nac);
+
+        //     const id_partido = fields[3];
+        //     const id_cargo = fields[4];
+
+        //     console.log("vamos dentro: ",id_candidato, nombres_candidato, fecha_nacimiento, id_partido, id_cargo);
+
+        //     //Insertar los datos en la tabla temporal
+        //     await db.querywithoutclose(connection, `INSERT INTO BD1PY1.CANDIDATOTMP (id_candidato, nombres_candidato, fecha_nacimiento, id_partido, id_cargo ) VALUES (?, ?, ?, ?, ?)`, [id_candidato, nombres_candidato, fecha_nacimiento, id_partido, id_cargo]);
+        // }
+        
+        // Cargar datos de partidos
+        // const datosPartidos = fs.readFileSync(filePath_Partidos, 'utf-8');
+        // const lines3 = datosPartidos.split('\n');
+        // for (let i = 1; i < lines3.length; i++) {
+        //     const fields = lines3[i].split(',');
+        //     const id_partido = fields[0];
+        //     const nombre_partido = fields[1];
+        //     const siglas = fields[2];
+        //     const fecha_fundac = fields[3];
+
+        //     //Convertir la fecha de fundacion
+        //     const fundacion = convertirFecha(fecha_fundac);
+
+
+        //     //Insertar los datos en la tabla temporal
+        //     await db.querywithoutclose(connection, `INSERT INTO BD1PY1.PARTIDOTMP (id_partido, nombre_partido, siglas, fundacion ) VALUES (?, ?, ?, ?)`, [id_partido, nombre_partido, siglas, fundacion]);
+        // }
+
+        // Cargar datos de cargos
+        const datosCargos = fs.readFileSync(filePath_Cargos, 'utf-8');
+        const lines4 = datosCargos.split('\n');
+        for (let i = 1; i < lines4.length; i++) {
+            const fields = lines4[i].split(',');
+            const id_cargo = fields[0];
+            const cargo = fields[1];
+
+            //Insertar los datos en la tabla temporal
+            await db.querywithoutclose(connection, `INSERT INTO BD1PY1.CARGOTMP (id_cargo, cargo ) VALUES (?, ?)`, [id_cargo, cargo]);
+        }
+
+        // Cargar datos de mesas
+        const datosMesas = fs.readFileSync(filePath_Mesas, 'utf-8');
+        const lines5 = datosMesas.split('\n');
+        for (let i = 1; i < lines5.length; i++) {
+            const fields = lines5[i].split(',');
+            const id_mesa = fields[0];
+            const id_departamento = fields[1];
+
+            //Insertar los datos en la tabla temporal
+            await db.querywithoutclose(connection, `INSERT INTO BD1PY1.MESATMP (id_mesa, id_departamento ) VALUES (?, ?)`, [id_mesa, id_departamento]);
+        }
+
+        // Cargar datos de departamentos
+        const datosDepartamentos = fs.readFileSync(filePath_Departamentos, 'utf-8');
+        const lines6 = datosDepartamentos.split('\n');
+        for (let i = 1; i < lines6.length; i++) {
+            const fields = lines6[i].split(',');
+            const id_departamento = fields[0];
+            const nombre_departamento = fields[1];
+
+            //Insertar los datos en la tabla temporal
+            await db.querywithoutclose(connection, `INSERT INTO BD1PY1.DEPARTAMENTOTMP (id_departamento, nombre_departamento ) VALUES (?, ?)`, [id_departamento, nombre_departamento]);
+        }
+
+        // Cargar datos de votaciones
+        const datosVotaciones = fs.readFileSync(filePath_Votaciones, 'utf-8');
+        const lines7 = datosVotaciones.split('\n');
+        for (let i = 1; i < lines7.length; i++) {
+            const fields = lines7[i].split(',');
+            const id_votacion = fields[0];
+            const id_candidato = fields[1];
+            const dpi = fields[2];
+            const id_mesa = fields[3];
+            const fechayhora = fields[4];
+
+            console.log(fechayhora);
+
+            //Insertar los datos en la tabla temporal
+            await db.querywithoutclose(connection, `INSERT INTO BD1PY1.VOTACIONESTMP (id_votacion, id_candidato, dpi, id_mesa, fechayhora ) VALUES (?, ?, ?, ?, ?)`, [id_votacion, id_candidato, dpi, id_mesa, fechayhora]);
+        }
+
+        // por ultimo pasamos los datos de la tabla temporal a la tabla original
+        //------------------------------------------------------------------------------------------
+
         await db.querywithoutclose(connection, `INSERT INTO BD1PY1.CIUDADANO (dpi, nombre, apellido, direccion, telefono, edad, genero) SELECT dpi, nombre, apellido, direccion, telefono, edad, genero FROM BD1PY1.CIUDADANOTMP`, []);
 
         // Cierra la conexión
@@ -73,4 +242,16 @@ exports.crearTabtemp = async (req, res) => {
             body: { res: false, message: 'Ocurrio un problema con la carga masiva :[', error },
         });
     }
+}
+
+
+function convertirFecha(fecha) {
+    const partes = fecha.split('/');
+    if (partes.length === 3) {
+        const dia = partes[0];
+        const mes = partes[1];
+        const anio = partes[2];
+        return `${anio}-${mes}-${dia}`;
+    }
+    return fecha; // Si no se puede convertir, devuelve la fecha original
 }
--- src/db/script.js
+++ src/db/script.js
@@ -14,8 +14,120 @@ CREATE TABLE IF NOT EXISTS BD1PY1.CIUDADANO (
   telefono VARCHAR(10) NOT NULL,
   edad VARCHAR(2) NOT NULL,
   genero VARCHAR(1) NOT NULL,
+
   PRIMARY KEY (dpi)
-  );`;
+
+);
+
+
+-- Tabla PARTIDO
+
+CREATE TABLE IF NOT EXISTS BD1PY1.PARTIDO (
+    id_partido INT NOT NULL AUTO_INCREMENT,
+    nombre_partido VARCHAR(50) NOT NULL,
+    siglas VARCHAR(10) NOT NULL,
+    fundacion DATE NOT NULL,
+
+    PRIMARY KEY (id_partido)
+);
+
+-- Tabla CARGO
+
+CREATE TABLE IF NOT EXISTS BD1PY1.CARGO (
+    id_cargo INT NOT NULL AUTO_INCREMENT,
+    cargo VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (id_cargo)
+);
+
+-- Tabla CANDIDATO
+    
+CREATE TABLE IF NOT EXISTS BD1PY1.CANDIDATO (
+    id_candidato INT NOT NULL AUTO_INCREMENT,
+    nombres VARCHAR(50) NOT NULL,
+    fecha_nac DATE NOT NULL,
+    id_partido INT NOT NULL,
+    id_cargo INT NOT NULL,
+
+    PRIMARY KEY (id_candidato),
+    FOREIGN KEY (id_partido) REFERENCES PARTIDO(id_partido),
+    FOREIGN KEY (id_cargo) REFERENCES CARGO(id_cargo)
+    
+);
+
+-- Tabla DEPARTAMENTO
+
+CREATE TABLE IF NOT EXISTS BD1PY1.DEPARTAMENTO (
+    id_departamento INT NOT NULL AUTO_INCREMENT,
+    nombre_departamento VARCHAR(50) NOT NULL,
+
+    PRIMARY KEY (id_departamento)
+);
+
+
+-- Tabla MESA
+
+CREATE TABLE IF NOT EXISTS BD1PY1.MESA (
+    id_mesa INT NOT NULL AUTO_INCREMENT,
+    id_departamento INT NOT NULL,
+
+    PRIMARY KEY (id_mesa),
+    FOREIGN KEY (id_departamento) REFERENCES DEPARTAMENTO(id_departamento)
+
+);
+
+
+
+-- Tabla VOTO
+
+CREATE TABLE IF NOT EXISTS BD1PY1.VOTO (
+    id_votacion INT NOT NULL AUTO_INCREMENT,
+    fechayhora DATE NOT NULL,
+    dpi VARCHAR(100) NOT NULL,
+    id_mesa INT NOT NULL,
+
+    PRIMARY KEY (id_votacion),
+    FOREIGN KEY (dpi) REFERENCES CIUDADANO(dpi),
+    FOREIGN KEY (id_mesa) REFERENCES MESA(id_mesa)
+);
+
+-- Tabla DETALLE_VOTO
+
+CREATE TABLE IF NOT EXISTS BD1PY1.DETALLE_VOTO (
+  id_detalle_voto INT NOT NULL AUTO_INCREMENT,
+  id_votacion INT NOT NULL,
+  id_candidato INT NOT NULL,
+
+  PRIMARY KEY (id_detalle_voto),
+  FOREIGN KEY (id_votacion) REFERENCES VOTO(id_votacion),
+  FOREIGN KEY (id_candidato) REFERENCES CANDIDATO(id_candidato)
+
+);
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  `;
 
 module.exports = script;
 
